61dd57af043f721b3fda101ae42102e56d476a87
Tian Zheng <tzheng@truman.edu>
2012-07-30 02:58:11 +0800

    more stuff

--- src/us/tzheng/JTetris/JGame.java
+++ src/us/tzheng/JTetris/JGame.java
@@ -4,29 +4,45 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class JGame extends JPanel implements KeyListener {
 
 	public JGame() {
-		
+		newPanel();
+		drawBoarder();
+		newCube();
+		Timer clock = new Timer(1000, new TimerListener());
+		clock.start();
 	}
 	
+	private void newPanel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void drawBoarder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void newCube() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		// no use
 	}
 
-	@Override
 	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		// no use
 	}
 
 }
---
+++ src/us/tzheng/JTetris/TimerListener.java
@@ -0,0 +1,14 @@
+package us.tzheng.JTetris;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TimerListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
